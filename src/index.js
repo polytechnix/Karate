573b8d0e683298014d6e4570a15e4c722a7e4d8c
polytechnix <polytechnix@yandex.ru>
2022-12-09 00:47:15 +0400

    added auxiliary elements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const GameViewport = {
 	WIDTH: 384,
 	HEIGHT: 224,
-	SCALE: 4
+	SCALE: 2
 };
 
 window.onload = function() {
 	const canvasSection = document.querySelector('canvas');
 	const canvasContext = canvasSection.getContext('2d');
 
-	canvasSection.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
-	canvasSection.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+	canvasSection.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
+	canvasSection.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+
+	canvasContext.strokeStyle = 'red';
+	canvasContext.moveTo(0, 0);
+	canvasContext.lineTo(canvasSection.width, canvasSection.height);
+	canvasContext.moveTo(canvasSection.width, 0);
+	canvasContext.lineTo(0, canvasSection.height);
+	canvasContext.stroke();
 }
