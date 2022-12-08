3aab6a83dd7c6d0636a351c5f60adc1027df39ff
polytechnix <polytechnix@yandex.ru>
2022-12-09 00:06:33 +0400

    continue experimenting with sprites

--- src/index.js
+++ src/index.js
@@ -4,36 +4,48 @@ const GameViewport = {
 };
 
 window.onload = function() {
-	// Constants
+	// Constants & Variables
 	const canvasSection = document.querySelector('canvas');
 	const canvasContext = canvasSection.getContext('2d');
 
 	canvasSection.width = GameViewport.WIDTH;
 	canvasSection.height = GameViewport.HEIGHT;
 
-	const player = document.querySelector('img');
+	const [player, background] = document.querySelectorAll('img');
 
 	const position = {
-		x: 0,
-		y: 0 
+		x: GameViewport.WIDTH / 2 - player.width / 2,
+		y: 120 
 	};
 
+	let velocity = 1;
+
 	// Helpers
-	canvasContext.strokeStyle = '#ac0202';
-
-	canvasContext.moveTo(0, 0);
-	canvasContext.lineTo(canvasSection.width, canvasSection.height);
-
-	canvasContext.moveTo(canvasSection.width, 0);
-	canvasContext.lineTo(0, canvasSection.height);
-
-	canvasContext.stroke();
+	function strokeSection() {
+		canvasContext.strokeStyle = '#ac0202';
+	
+		canvasContext.moveTo(0, 0);
+		canvasContext.lineTo(canvasSection.width, canvasSection.height);
+	
+		canvasContext.moveTo(canvasSection.width, 0);
+		canvasContext.lineTo(0, canvasSection.height);
+	
+		canvasContext.stroke();
+	}
 
 	function frame() {
-		position.x++;
-		canvasContext.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+		position.x += velocity;
+		
+		if(position.x > canvasSection.width || position.x > 0) {
+			velocity = -velocity;
+		}
+
+		// canvasContext.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+		strokeSection();
 	
 		canvasContext.drawImage(player, 0, 0);
+		canvasContext.drawImage(background, 0, 0);
+
 		window.requestAnimationFrame(frame);
 	}
 
