af07ffef0c57bbe5ef68ca18b952b3c879bf47cf
polytechnix <polytechnix@yandex.ru>
2022-12-08 23:55:14 +0400

    added a sprite (working with a sprite)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 const GameViewport = {
 	WIDTH: 384,
-	HEIGHT: 224,
-	SCALE: 2
+	HEIGHT: 224
 };
 
 window.onload = function() {
+	// Constants
 	const canvasSection = document.querySelector('canvas');
 	const canvasContext = canvasSection.getContext('2d');
 
-	canvasSection.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
-	canvasSection.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+	canvasSection.width = GameViewport.WIDTH;
+	canvasSection.height = GameViewport.HEIGHT;
 
-	canvasContext.strokeStyle = 'red';
+	const player = document.querySelector('img');
+
+	const position = {
+		x: 0,
+		y: 0 
+	};
+
+	// Helpers
+	canvasContext.strokeStyle = '#ac0202';
+
 	canvasContext.moveTo(0, 0);
 	canvasContext.lineTo(canvasSection.width, canvasSection.height);
+
 	canvasContext.moveTo(canvasSection.width, 0);
 	canvasContext.lineTo(0, canvasSection.height);
+
 	canvasContext.stroke();
+
+	function frame() {
+		position.x++;
+		canvasContext.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+	
+		canvasContext.drawImage(player, 0, 0);
+		window.requestAnimationFrame(frame);
+	}
+
+	window.requestAnimationFrame(frame);
 }
